ba35616aa3a9a7ff84f12bd6ae8c7ce5f540a914
c212 <40395648+c212@users.noreply.github.com>
2019-09-05 15:04:44 -0400

    second stage
    small advance

--- demo-assignment/Generator.java
+++ demo-assignment/Generator.java
@@ -1,3 +1,12 @@
 public class Generator {
-  
+  public Generator() { } // I could have used the default 
+  public void addRange() { } 
+  public String nextString(int size) { return "haha"; }
+  public static void main(String[] args) {
+    Generator a = new Generator(); 
+    a.addRange(); 
+    a.addRange(); 
+    String result = a.nextString(10); 
+    System.out.println( result ); 
+  }
 }
